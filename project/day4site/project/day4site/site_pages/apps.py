4a8da2b2181769c729174d49085d6856091a90e5
Ayman khan <khanayman4786@gmail.com>
2026-05-07 01:11:02 +0530

    Added CSS to All Pages

--- project/day4site/project/day4site/site_pages/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class SitePagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'site_pages'
-
-    def ready(self):
-        import site_pages.signals
